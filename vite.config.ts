d3b8764a9d745e0c8701ec1cd87211f48f571e28
04042015 <m.iggopramulia14@gmail.com>
2025-07-19 02:36:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // <-- Pindahkan ke sini (root level)
   server: {
     host: "::",
     port: 8080,
-    base: './'
   },
   plugins: [
     react(),
