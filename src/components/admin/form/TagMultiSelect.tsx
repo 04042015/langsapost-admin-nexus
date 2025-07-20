2184f524bfc2161d543bfc91c027a76d83907a6f
04042015 <m.iggopramulia14@gmail.com>
2025-07-21 01:53:37 +0700

    Update TagMultiSelect.tsx

--- src/components/admin/form/TagMultiSelect.tsx
+++ src/components/admin/form/TagMultiSelect.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabase-client";
+import { supabase } from '@/lib/supabase'
 import { MultiSelect } from "@/components/ui/multi-select";
 
 export interface TagOption {
