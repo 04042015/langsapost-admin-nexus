70f3e9b44fd9fb074d8e0dce7e331b2cf47acc2b
04042015 <m.iggopramulia14@gmail.com>
2025-07-20 17:13:16 +0700

    Update GalleryImageUpload.tsx

--- src/components/admin/form/GalleryImageUpload.tsx
+++ src/components/admin/form/GalleryImageUpload.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useState } from "react";
 
 interface GalleryImageUploadProps {
